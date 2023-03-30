8633f6f823718654a31d0ecae7c867f7c7ad5c2e
hannah pelingon <hannahpelingon@hannahs-MacBook-Pro.local>
2023-03-30 11:37:21 -0400

    lab 5.1 added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [userEmail, setUserEmail] = useState();
+  const [inputInvalid, setInputInvalid] = useState(false);
+
+  function userEmailChangeHandler(event) {
+    setUserEmail(event.target.value);
+  }
+
+  function submitFormHandler(event){
+    event.preventDefault();
+    const emailCheckValid = userEmail.includes('@');
+    setInputInvalid(!emailCheckValid);
+  }
+
   return (
     <div className="App">
-      
+      <form onSubmit={submitFormHandler}>
+        <label>Your Email: </label>
+        <input typ="text" id="email" onChange={userEmailChangeHandler}></input>
+        <button>Submit</button>
+      </form>
+      {inputInvalid && <p>Invalid email address entered.</p>}
     </div>
   );
 }
